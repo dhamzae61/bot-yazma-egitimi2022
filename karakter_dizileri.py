db59b8b6c05f883c87f1418468d2b8250d4a221b
dhamzae61 <114801784+dhamzae61@users.noreply.github.com>
2022-10-15 11:32:47 +0300

    Create karakter_dizileri.py

---
+++ karakter_dizileri.py
@@ -0,0 +1,21 @@
+# Kullanıcı yaş ve isim değişkenlerini oluşturalım
+yas= 14
+isim= "hamza"
+
+# kullanıcı bilgilerini ekrana yazdıralım
+#adınız hamza yaşınız 14
+x= input("adınız: ")
+y = input("yaşınız: ")
+print("adınız: " + x, " , yaşınız: " + y)
+
+# karakter verisinin uzunluğuna bakalım
+print(len(isim))
+
+# str veri tipindeki değerin çeşitli elemanlarını yazdıralım
+#elemanlar 0 dan başlar
+print(isim[o])
+print(isim[1])
+print(isim[2])
+print(isim[3])
+
+
